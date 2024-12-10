0578ccfad82ebb3b3681369b91c70327be9456f0
Matyasuss <113213564+Matyasuss@users.noreply.github.com>
2024-12-10 11:09:42 +0100

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,17 @@
 package org.example;
 
 import com.google.inject.Inject;
+import org.example.accounts.BankAccount;
 import org.example.accounts.BankAccountFacade;
+import org.example.accounts.BankAccountFactory;
+import org.example.accounts.SavingsAccount;
 import org.example.accounts.interest.InterestingService;
+import org.example.cards.BankCard;
+import org.example.cards.BankCardFactory;
 import org.example.people.Owner;
 import org.example.people.OwnerFactory;
 import org.example.people.OwnerJsonSerializationService;
 import org.example.print.AccountDetailPrinter;
-
-import java.util.Date;
 
 public class App {
     public void run() {
@@ -17,6 +20,9 @@ public class App {
 
     @Inject
     OwnerFactory ownerFactory;
+
+    @Inject
+    BankAccountFactory bankAccountFactory;
 
     @Inject
     BankAccountFacade bankAccountFacade;
@@ -33,9 +39,25 @@ public class App {
     @Inject
     InterestingService interestingService;
 
+    @Inject
+    BankCardFactory debitCardFactory;
+
     void runBank() {
-        Owner owner = ownerFactory.createOwner("Pepa", "Doe", "11");
+        Owner owner1 = ownerFactory.createOwner("Pepa", "Doe", "11");
 
+        BankAccount bankAccount = bankAccountFacade.createBankAccount(100, owner1);
+        BankAccount firstBankAccount = this.bankAccountFactory.createBankAccount(1000, owner1);
+        // create a payment card
+        BankCard paymentCard = this.debitCardFactory.createDebitCard();
 
+        firstBankAccount.associateBankCard(paymentCard);
+
+        accountDetailPrinter.printDetail(bankAccount);
+
+        SavingsAccount savingsAccount = bankAccountFacade.createSavingBankAccount(1000, owner1, "4321");
+        // add interest
+        this.interestingService.calculate();
+
+        accountDetailPrinter.printDetail(savingsAccount);
     }
 }
